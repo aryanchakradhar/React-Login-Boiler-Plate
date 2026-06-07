5290a4d8a42c085b8cdb354ee9fd8f90d64d0a5c
aryanchakradhar <aryanchakradhar11@gmail.com>
2026-06-07 20:16:14 +0545

    feat: Setup the routing using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login-page";
+import { pageRoutes } from "./libs/constants/page-constants";
+
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: pageRoutes.login,
+      element: <LoginPage/>
+    }
+  ])
 
   return (
-    <>
-    Install dependecies
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/libs/constants/page-constants.ts
@@ -0,0 +1,3 @@
+export const pageRoutes = {
+  login: "/login",
+};
